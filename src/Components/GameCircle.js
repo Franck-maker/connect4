fc533e738ff6575938577467e210163b386feaec
Franck-maker <125270395+Franck-maker@users.noreply.github.com>
2023-12-08 11:53:17 +0100

    New commit

--- src/Components/GameCircle.js
+++ src/Components/GameCircle.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const onClick =(id) => {      // learn how to pass one or more parameters into a react event
+const onClick =(ev,id) => {      // learn how to pass one or more parameters into a react event
     debugger;
     alert('on Click' + id);
 }
 const GameCircle = ({id, color, children}) => {
     console.log(id);
     return (
-        <div style={{backgroundColor: color}} onClick={() => onClick(id)}>  
+        <div style={{backgroundColor: color, width: 100, height: 100, margin: 100}} onClick={(ev) => onClick(id, ev)}>  
             {children}
         </div>
     )
